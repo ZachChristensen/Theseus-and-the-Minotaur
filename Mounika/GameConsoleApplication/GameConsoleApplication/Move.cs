c1debca9238991988517fad4b1ebcc3d4b6cfab7
mounikaymrs <moy0037@X303-53476.cpit.ac.nz>
2015-08-17 08:56:58 +1200

    New folder added

---
+++ Mounika/GameConsoleApplication/GameConsoleApplication/Move.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameConsoleApplication
+{
+    public static class Move
+    {
+        public static Point Up = new Point(0, -1);
+        public static Point Down = new Point(0, 1);
+        public static Point Left = new Point(-1, 0);
+        public static Point Right = new Point(1, 0);
+    }
+}
---
+++ Mounika/GameConsoleApplication/GameConsoleApplication/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+namespace GameConsoleApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Maze game = new Maze();
+            Thing theseusPosition = new Thing(0, 2);
+            Point theseusCurrentPosition = new Point(0, 2);
+            game.minotaur = new Minotaur(1, 0);
+            game.CreateMaze();
+            game.MoveTheseus(game.GetTheseusNextMove());
+            Console.ReadLine();
+        }
+    }
+}
