6d4672eb8db0bc3ae5140d8c1ee59d48832145fa
midas06 <limh@T120-54202.cpit.ac.nz>
2015-08-17 15:15:27 +1200

    Harry

---
+++ Harry/TheseusMinotaur/TheseusMinotaur/Game.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheseusMinotaur
+{
+    class Game
+    {
+        Minotaur minotaur;
+        Theseus theseus;
+        Tile[,] Map1;
+
+        public void CreateMap1()
+        {
+       
+        }
+         
+    }
+}
---
+++ Harry/TheseusMinotaur/TheseusMinotaur/Wall.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheseusMinotaur
+{
+    [Flags]
+    enum TheWalls
+    {
+        None = 0x0,
+        North = 0x1,
+        East = 0x2,
+        South = 0x4,
+        West = 0x8,
+        End = 0x10
+    }
+    class Wall
+    {
+    }
+}
