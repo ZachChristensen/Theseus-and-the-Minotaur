ca547fadaaf0d24f7026d7bca548e64cfdecab95
midas06 <limh@T120-54202.cpit.ac.nz>
2015-08-17 15:05:03 +1200

    Main commit

---
+++ Main/TheseusMinotaur/TheseusMinotaur/Thing.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TheseusMinotaur
+{
+   
+    class Thing
+    {
+        public struct Coordinate
+        {
+            byte x;
+            byte y;
+            public Coordinate(byte newX, byte newY)
+            {
+                x = newX;
+                y = newY;
+            }
+        }
+    }
+}
