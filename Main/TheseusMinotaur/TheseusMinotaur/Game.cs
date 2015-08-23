9006bd8bfb4fc5c51d64292efaec5bece8a09a03
Mounika <mounikaymrs@gmail.com>
2015-08-23 14:21:49 +1200

    CreateMap1() fixed and WIP character movement functionality added

--- Main/TheseusMinotaur/TheseusMinotaur/Game.cs
+++ Main/TheseusMinotaur/TheseusMinotaur/Game.cs
@@ -13,38 +13,84 @@ namespace TheseusMinotaur
         Theseus theseus;
         Tile[,] Map1;
 
-        public void CreateMap1() //UNTESTED -- doesn't work, need to create each tile separately - Harry
+        public void CreateMap1() //UNTESTED -- doesn't work, need to create each tile separately - Harry 
         {
             Map1 = new Tile[4, 3];
             //[x,y] TheWalls.North | TheWalls.South |TheWalls.West | TheWalls.East |TheWalls.End
-            Map1[0, 0] = new Tile(TheWalls.North | TheWalls.West);
-            Map1[1, 0] = new Tile(TheWalls.North);
-            Map1[2, 0] = new Tile(TheWalls.North | TheWalls.East);
-            Map1[3, 0] = new Tile(TheWalls.None);
+            Map1[0, 0] = new Tile(0, 0);
+            Map1[0, 0].MyWalls.HasFlag(TheWalls.North | TheWalls.West);
 
-            Map1[0, 1] = new Tile(TheWalls.West);
-            Map1[1, 1] = new Tile(TheWalls.North | TheWalls.South | TheWalls.East);
-            Map1[2, 1] = new Tile(TheWalls.None);
-            Map1[3, 1] = new Tile(TheWalls.North | TheWalls.South | TheWalls.East | TheWalls.End);
+            Map1[1, 0] = new Tile(1, 0);
+            Map1[1, 0].MyWalls.HasFlag(TheWalls.North);
 
-            Map1[0, 2] = new Tile(TheWalls.South | TheWalls.West);
-            Map1[1, 2] = new Tile(TheWalls.South);
-            Map1[2, 2] = new Tile(TheWalls.South | TheWalls.East);
-            Map1[3, 2] = new Tile(TheWalls.None);
+            Map1[2, 0] = new Tile(2, 0);
+            Map1[2, 0].MyWalls.HasFlag(TheWalls.North | TheWalls.East);
+
+            Map1[3, 0] = new Tile(3, 0);
+            Map1[3, 0].MyWalls.HasFlag(TheWalls.None);
+
+            Map1[0, 1] = new Tile(0, 1);
+            Map1[0, 1].MyWalls.HasFlag(TheWalls.West);
+
+            Map1[1, 1] = new Tile(1, 1);
+            Map1[1, 1].MyWalls.HasFlag(TheWalls.North | TheWalls.South | TheWalls.East);
+
+            Map1[2, 1] = new Tile(2, 1);
+            Map1[2, 1].MyWalls.HasFlag(TheWalls.None);
+
+            Map1[3, 1] = new Tile(3, 1);
+            Map1[3, 1].MyWalls.HasFlag(TheWalls.North | TheWalls.South | TheWalls.East | TheWalls.End);
+
+            Map1[0, 2] = new Tile(0, 2);
+            Map1[0, 2].MyWalls.HasFlag(TheWalls.South | TheWalls.West);
+
+            Map1[1, 2] = new Tile(1, 2);
+            Map1[1, 2].MyWalls.HasFlag(TheWalls.South);
+
+            Map1[2, 2] = new Tile(2, 2);
+            Map1[2, 2].MyWalls.HasFlag(TheWalls.South | TheWalls.East);
+
+            Map1[3, 2] = new Tile(3, 2);
+            Map1[3, 2].MyWalls.HasFlag(TheWalls.None);
         }
 
-        public bool TheseusTurn()//return true if he moves to the exit
+        public bool MoveCharacter(Character character, Point direction)
         {
-
-            return false;
+            if (IsWall(character, direction))
+            {
+                return false;
+            }
+            else
+            {
+                // Update current position of character
+                character.Coordinate.X += direction.X;
+                character.Coordinate.Y += direction.Y;
+                return true;
+            }
         }
+
+        public bool TheseusTurn(Point direction)//return true if he moves to the exit - return true only in this case??
+        {
+            if (!direction.IsEmpty)
+            {
+                return MoveCharacter(theseus, direction);
+            }
+            else
+            {
+                return false;
+            }
+        }
+
 
         public bool MinotaurTurn()//return false if it catches theseus
         {
-
             return true;
         }
-
+        public bool IsWall(Character character, Point direction) // TODO:
+        {
+            return false;
+        }
+    
         public void Play()
         {
             CreateMap1();
