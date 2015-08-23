f15456d7ab23c0a0f0bbb6de6d4d3a27fd402698
MisaSekihara <misa95@gmail.com>
2015-08-23 12:22:58 +1200

    Added Ascii Map & Escape/Kill Theseus method
    -Mounika, I'm sorry I somehow overwritten your files so I reverted my commit. It should be fine now, sorry.

--- Misa/MinoThesGameConsoleApp/Program.cs
+++ Misa/MinoThesGameConsoleApp/Program.cs
@@ -6,27 +6,9 @@ namespace MinoThesGameConsoleApp
     {
         static void Main(string[] args)
         {
-            Game game = new Game();
-
-            game.Initialise();  // Creates a lv.1 map, put Theseus and Minotaur in the game 
-            game.PrintGridCoordination();   // Print the point coordinations for easy visualalisation of the map
-
-
-            // Test Test Test
-            Console.WriteLine("Minotaur default locaion{0}", game.Minotaur.Position);
-            Console.WriteLine("Theseus's current locaion{0}\n", game.Theseus.Position);
+            Game game = new Game();         
 
             game.Play();
-
-/*            Console.WriteLine("Theseus's next locaion {X=2, Y=2}\n");
-
-            Console.WriteLine("Minotaur's expected location {X=2, Y=1}\n");
-
-            game.MoveMinotaur(game.Theseus.Position);
-            Console.WriteLine("Minotaur move 1 locaion{0}", game.Minotaur.Position);
-
-            game.MoveMinotaur(game.Theseus.Position);
-            Console.WriteLine("Minotaur move 2 locaion{0}", game.Minotaur.Position);*/
 
             Console.ReadLine();
         }
